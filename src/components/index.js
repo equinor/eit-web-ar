085615729bad5dbb933ae86e68adb170abe50e84
Eric Thörn <eric.m.thorn@gmail.com>
2020-06-23 09:44:24 +0200

    Trying to get aframe components to work

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@
 // import components here to make webpack detect it
 
 import "./test-box";
-import "./change-color-on-hover"
-import "./events.js"
+import "./change-color-on-hover";
+import "./events";
