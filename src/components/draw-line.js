83f5cf5ed2440f3b41c917b88716ae493d6133de
Erlend Blomseth <erlend@elendig.no>
2020-06-24 08:54:00 +0200

    poc draw lines between markers

--- src/components/draw-line.js
+++ src/components/draw-line.js
@@ -5,45 +5,36 @@ const log = utils.getLogger("components:draw-line");
 AFRAME.registerComponent('draw-line', {
   schema: {},
   init: function () {
+    // Maintain a list of active markers
     this.markersVisible = [];
     this.el.sceneEl.addEventListener('markerFound', (e) => {
-      this.markersVisible.push(e.target);
-
-      if (this.markersVisible.length > 2) {
-        //draw lines
-        console.log(this.markersVisible[0]);
-        console.log(this.markersVisible[0].getAttribute('position'));
-
-        var material = new THREE.LineBasicMaterial( { color: '#f00' } );
-        var points = [
-          this.markersVisible[0].getAttribute('position'),
-          this.markersVisible[1].getAttribute('position'),
-          this.markersVisible[2].getAttribute('position')
-        ];
-        var geometry = new THREE.BufferGeometry().setFromPoints(points);
-        var line = new THREE.Line(geometry, material);
-        this.el.sceneEl.object3D.add(line);
-        renderer.render(this.el.sceneEl, this.el.sceneEl.querySelector('camera'))
+      if (!this.markersVisible.includes(e.target.id)) {
+        this.markersVisible.push(e.target.id);
         console.log(this.markersVisible);
       }
     });
-
-  }/*,
-  update: function() {
-    log.info('init');
-    this.el.addEventListener('markerFound', (e) => {
-      log.info('marker found');
-      // loop through all markers and draw lines
-      var markers = document.querySelectorAll('a-marker');
-      var markersVisible = [];
-
-      for (var i = 0; i < markers.length; i++) {
-        if (markers[i].object3D.visible) {
-          // todo: skip this marker
-          markersVisible.push(markers[i]);
+    this.el.sceneEl.addEventListener('markerLost', (e) => {
+      for (var i = 0; i < this.markersVisible.length; i++) {
+        if (e.target.id == this.markersVisible[i]) {
+          this.markersVisible.pop(i);
+          break;
         }
       }
-      console.log(markersVisible);
+      console.log(this.markersVisible);
     });
-  }*/
+  },
+  tick: function () {
+    if (this.markersVisible.length > 1) {
+      var material = new THREE.LineBasicMaterial( { color: '#f00' } );
+      var points = [];
+      for (var i = 0; i < this.markersVisible.length; i++) {
+        var markerId = this.markersVisible[i];
+        points.push(document.getElementById(markerId).getAttribute('position'));
+      }
+      var geometry = new THREE.BufferGeometry().setFromPoints(points);
+      var line = new THREE.Line(geometry, material);
+      this.el.sceneEl.object3D.add(line);
+      this.el.sceneEl.renderer.render(this.el.sceneEl, this.el.sceneEl.querySelector('a-camera').object3D);
+    }
+  }
 });
