34e352c2a2d3bcb9e8d518b5bc0dac7ec32a8ced
olaals <olaalstad96@gmail.com>
2020-06-26 11:24:20 +0200

    New id's and refactor css

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -2,22 +2,20 @@
 var toggle = true;
 
 document.addEventListener("DOMContentLoaded", function(){
-    doubleup_el = document.getElementById("doubleup");
-    one_el = document.getElementById("one");
-    two_el = document.getElementById("two");
-    three_el = document.getElementById("three")
+    doubleup_el = document.getElementById("button_doubleup");
+    one_el = document.getElementById("button_one");
+    two_el = document.getElementById("button_two");
+    three_el = document.getElementById("button_three")
 
 
     doubleup_el.addEventListener("click", function(){
         if(toggle){
-            console.log("toggle off")
             one_el.style.display = "block";
             two_el.style.display = "block";
             three_el.style.display = "block";
             toggle = false;
             doubleup_el.style.transform = "scaleY(-1)"
         }else{
-            console.log("toggle on")
             toggle = true;
             one_el.style.display = "none";
             two_el.style.display = "none";
