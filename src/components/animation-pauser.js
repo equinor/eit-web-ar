fc465dbc140f2b580dbea9087b24c3984efa2a69
Jonas Rogde Jørgensen <jonasrj@stud.ntnu.no>
2020-07-06 11:45:49 +0200

    Add dependencies + relocated play variable

--- src/components/animation-pauser.js
+++ src/components/animation-pauser.js
@@ -1,10 +1,14 @@
 import AFRAME from "aframe"
 import * as utils from "../modules/utils";
 const log = utils.getLogger("components:animation-pauser");
-var play = 1;
 
 AFRAME.registerComponent('animation-pauser', {
+  dependencies: [
+    'cursor-interactive',
+    'animation-mixer'
+  ],
   init: function() {
+    var play = 1;
     this.el.addEventListener('click', function(e) {
       if (play == 0){
         this.setAttribute('animation-mixer', {timeScale: 1});
