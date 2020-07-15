ed1fce51f20154013aa5df32884cef87ce952b0a
Erlend Blomseth <erlend.blomseth@gmail.com>
2020-07-15 08:45:02 +0200

    Socket baseuri (#231)
    * Add socketUri to api object
    
    * Change api.baseUri to api.socketUri in frontend/game.js

--- frontend/src/modules/api.js
+++ frontend/src/modules/api.js
@@ -9,8 +9,20 @@ function getApiUri () {
   }
 }
 
+function getSocketUri () {
+  let dockerUri = `http://${window.location.hostname}:3100`;
+  let productionUri = `https://${window.location.hostname}`;
+  
+  if (window.location.hostname.indexOf("localhost") > -1) {
+    return dockerUri;
+  } else {
+    return productionUri;
+  }
+}
+
 const api = {
-  baseUri: getApiUri()
+  baseUri: getApiUri(),
+  socketUri: getSocketUri()
 };
 
 export default api;
