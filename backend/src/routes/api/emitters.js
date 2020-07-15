ce862cc50d1cb05a08fb57e69fd0c07cbb77f872
Erlend Blomseth <erlend@elendig.no>
2020-07-15 13:13:23 +0200

    fix emitters module

--- backend/src/routes/api/emitters.js
+++ backend/src/routes/api/emitters.js
@@ -1,56 +1,56 @@
 var storage = require('../../modules/storage');
 
-function emitters.emitEntitiesUpdated(io, playerId, entities) {
-  io.emit('entities-updated', {
-    playerId: playerId,
-    entities: entities
-  });
-});
-
-function emitters.emitPlayerAdded(io, playerId, name) {
-  io.emit('player-added', {
-    playerId: playerId,
-    name: name
-  });
-}
-
-function emitters.emitGameStarted(io) {
-  io.emit('status-change', {
-    status: "running"
-  });
-}
-
-function emitters.emitEntitiesUpdated(io, playerId, entities) {
-  io.emit('entities-updated', {
-    playerId: playerId,
-    entities: entities
-  });
-}
-
-function emitters.emitGameOver(io) {
-  io.emit('status-change', {
-    status: 'game-over'
-  });
-}
-
-
-function emitters.emitEntitySent(io, fromPlayerId, toPlayerId) {
-  const multi = [
-    ['hget', utils.getPlayerHash(fromPlayerId), 'name'],
-    ['hget', utils.getPlayerHash(toPlayerId), 'name']
-  ];
-  storage.multi(multi).exec(function(err, playerNames) {
-    io.emit('entity-sent', {
-      fromPlayer: {
-        playerId: fromPlayerId,
-        name: playerNames[0]
-      },
-      toPlayer: {
-        playerId: toPlayerId,
-        name: playerNames[1]
-      }
+module.exports = {
+  
+  emitEntitiesUpdated: function(io, playerId, entities) {
+    io.emit('entities-updated', {
+      playerId: playerId,
+      entities: entities
     });
-  });
-}
+  },
 
-module.exports = emitters;
+  emitPlayerAdded: function(io, playerId, name) {
+    io.emit('player-added', {
+      playerId: playerId,
+      name: name
+    });
+  },
+
+  emitGameStarted: function(io) {
+    io.emit('status-change', {
+      status: "running"
+    });
+  },
+
+  emitEntitiesUpdated: function(io, playerId, entities) {
+    io.emit('entities-updated', {
+      playerId: playerId,
+      entities: entities
+    });
+  },
+
+  emitGameOver: function(io) {
+    io.emit('status-change', {
+      status: 'game-over'
+    });
+  },
+
+  emitEntitySent: function(io, fromPlayerId, toPlayerId) {
+    const multi = [
+      ['hget', utils.getPlayerHash(fromPlayerId), 'name'],
+      ['hget', utils.getPlayerHash(toPlayerId), 'name']
+    ];
+    storage.multi(multi).exec(function(err, playerNames) {
+      io.emit('entity-sent', {
+        fromPlayer: {
+          playerId: fromPlayerId,
+          name: playerNames[0]
+        },
+        toPlayer: {
+          playerId: toPlayerId,
+          name: playerNames[1]
+        }
+      });
+    });
+  }
+};
