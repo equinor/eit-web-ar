ed9e62ae533e4cb49b5a973a3cdd949d37e33a75
Erlend Blomseth <erlend@elendig.no>
2020-07-22 14:52:02 +0200

    Lots of frontend changes. Users can now see each other

--- frontend/src/components/meeting.js
+++ frontend/src/components/meeting.js
@@ -6,35 +6,56 @@ import meeting from '../modules/meeting';
 AFRAME.registerComponent('meeting', {
   init: function() {
     const _this = this;
-    const name = 'testing'
-    meeting.registerUser(name, '', (userId) => {
-      log.info('User registered with name ' + name + ' and id ' + userId);
-      meeting.setName(name);
-      meeting.setUserId(userId);
-      this.startEmittingPosition();
+    const properties = {
+      name: meeting.getRandomName(),
+      color: meeting.getRandomColor()
+    };
+    meeting.registerUser(properties, (response) => {
+      log.info('I am now registered with name ' + response.name + ', id ' + response.userId);
+      meeting.setMyUserId(response.userId);
+      meeting.setUserProperties(response.userId, response); //?
+      meeting.connectSocket();
+      window.addEventListener('gps-camera-update-position', function (e) {
+        meeting.emitPosition(e.detail.position.latitude, e.detail.position.longitude);
+      });
+    });
+    
+    meeting.requestAllUsers(users => {
+      for (let i = 0; i < users.length; i++) {
+        meeting.setUserProperties(users[i].userId, users[i]);
+      }
     });
     
     meeting.receivePositions(data => {
       console.log('received position-update:');
       console.log(data);
       for (let i = 0; i < data.length; i++) {
-        let findUserEl = document.querySelector(`[data-userId="${data[i].userId}"]`);
+        const userId = data[i].userId;
+        if (meeting.getUserProperties(userId) === undefined) break;
+        const latitude = data[i].latitude;
+        const longitude = data[i].longitude;
+        let findUserEl = document.querySelector(`[data-userId="${userId}"]`);
         if (!findUserEl) {
-          console.log('appending ' + data[i].userId);
+          console.log('Appending ' + userId + ' entity.');
           let userEl = document.createElement('a-entity');
-          userEl.setAttribute('data-userId', data[i].userId);
-          userEl.setAttribute('gps-entity-place', `latitude: ${data[i].latitude}; longitude: ${data[i].longitude}`);
-          userEl.setAttribute('geometry', 'primitive', 'sphere');
-          userEl.setAttribute('material', 'color', '#f00');
+          const geometry = meeting.getUserProperties(userId).geometry;
+          const color = meeting.getUserProperties(userId).color;
+          userEl.setAttribute('data-userId', userId);
+          userEl.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+          userEl.setAttribute('geometry', 'primitive', geometry);
+          userEl.setAttribute('material', 'color', color);
           userEl.setAttribute('scale', '0.3 0.3 0.3');
           document.querySelector('a-scene').appendChild(userEl);
+        } else {
+          findUserEl.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
         }
       }
     });
     
     meeting.receiveUserJoined(data => {
       console.log('received user-joined:');
-      console.log(data);
+      meeting.setUserProperties(data.userId, data);
+      console.log(meeting.getUserProperties(data.userId));
     });
     meeting.receiveUserLeft(data => {
       console.log('received user-left:');
@@ -47,11 +68,6 @@ AFRAME.registerComponent('meeting', {
     meeting.receiveUserLeftGroup(data => {
       console.log('received user-left-group:');
       console.log(data);
-    });
-  },
-  startEmittingPosition: function() {
-    window.addEventListener('gps-camera-update-position', function (e) {
-      meeting.emitPosition(e.detail.position.latitude, e.detail.position.longitude);
     });
   }
 });
--- frontend/src/modules/meeting.js
+++ frontend/src/modules/meeting.js
@@ -29,8 +29,14 @@ const api = {
   socketUri: getSocketUri()
 }
 
-var name = '';
-var userId = 0;
+const defaultUserProperties = {
+  userId: 0,
+  name: 'DefaultName',
+  geometry: 'sphere',
+  color: '#f00'
+}
+var userProperties = [];
+var myUserId;
 
 const socket = io(api.socketUri);
 socket.on('connect', function(data){
@@ -38,13 +44,8 @@ socket.on('connect', function(data){
 });
 
 module.exports = {
-  registerUser: function (name, avatar, callback) {
+  registerUser: function (properties, callback) {
     const regUrl = api.baseUri + '/user';
-
-    const payload = {
-      name: name,
-      avatar: ''
-    };
 
     axios({
       method: 'post',
@@ -53,14 +54,14 @@ module.exports = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       },
-      data: payload
+      data: properties
     })
       .then((response) => {
         if (response.status == 201) {
-          callback(response.data.userId);
+          callback(response.data);
           return true;
         } else {
-          console.log('Something went wrong when registering');
+          console.log('Error with post /user');
           return false;
         }
       })
@@ -68,13 +69,27 @@ module.exports = {
         console.error(error);
       });
   },
-  setName: function(name) {
-    name = name;
+  setMyUserId: function(userId) {
+    myUserId = userId;
   },
-  setUserId: function(userId) {
-    userId = userId;
+  getMyUserId: function() {
+    return myUserId;
+  },
+  setUserProperties: function(userId, properties) {
+    userProperties[userId] = {};
+    Object.assign(userProperties[userId], properties);
+    for (let [key, value] of Object.entries(defaultUserProperties)) {
+      if (!userProperties[userId].hasOwnProperty(key)) {
+        userProperties[userId][key] = value;
+      }
+    }
+  },
+  getUserProperties: function(userId) {
+    return userProperties[userId];
+  },
+  connectSocket: function() {
     socket.emit('connect-socket-to-user', {
-      userId: userId
+      userId: myUserId
     });
   },
   emitPosition: function(latitude, longitude) {
@@ -109,5 +124,46 @@ module.exports = {
     socket.on('user-left-group', data => {
       callback(data);
     });
+  },
+  getRandomColor: function() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  },
+  getRandomName: function() {
+    let letters = 'abcdefghijklmnopqrstuvwxyz';
+    let name = '';
+    for (let i = 0; i < 6; i++) {
+      name += letters[Math.floor(Math.random() * 26)];
+    }
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+    return name;
+  },
+  requestAllUsers: function(callback) {
+    const regUrl = api.baseUri + '/users';
+
+    axios({
+      method: 'get',
+      url: regUrl,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          callback(response.data);
+          return true;
+        } else {
+          console.log('Error with get /users');
+          return false;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
